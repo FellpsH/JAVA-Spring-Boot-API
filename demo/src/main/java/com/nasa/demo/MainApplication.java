45f78838f747a6f3bd12682e923ee5cf1483ed15
Fellipe Henrique Babeto Oliveira <60347958+FellpsH@users.noreply.github.com>
2024-04-17 09:05:44 -0300

    Update MainApplication.java

--- demo/src/main/java/com/nasa/demo/MainApplication.java
+++ demo/src/main/java/com/nasa/demo/MainApplication.java
@@ -1,5 +1,4 @@
 package com.nasa.demo;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
